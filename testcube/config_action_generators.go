da4dc55055f9f2df1a56b99191d18a63eb415d26
reus <reusee@gmail.com>
2021-12-10 17:21:15 +0800

    rename ActionNoop to ActionNoOP

--- testcube/config_action_generators.go
+++ testcube/config_action_generators.go
@@ -4,9 +4,9 @@ import "github.com/reusee/fz"
 
 func (_ ConfigScope) ActionGenerators() fz.ActionGenerators {
 	return fz.ActionGenerators{
-		//TODO
 		func() fz.Action {
-			return fz.Seq()
+			return ActionNoOP{}
 		},
+		//TODO
 	}
 }
---
+++ testcube/config_actions.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/reusee/fz"
+
+func init() {
+	fz.RegisterAction(ActionNoOP{})
+}
+
+type ActionNoOP struct{}
