a3ea2656cca8140e98a830fe3bf06a4a34c8dda6
reus <reusee@gmail.com>
2021-12-16 12:01:11 +0800

    testcube: update cube init

--- testcube/cube_cluster_test.go
+++ testcube/cube_cluster_test.go
@@ -7,13 +7,15 @@ import (
 	"testing"
 	"time"
 
+	crdbpebble "github.com/cockroachdb/pebble"
 	prophetconfig "github.com/matrixorigin/matrixcube/components/prophet/config"
 	"github.com/matrixorigin/matrixcube/components/prophet/util/typeutil"
 	"github.com/matrixorigin/matrixcube/config"
+	"github.com/matrixorigin/matrixcube/raftstore"
 	"github.com/matrixorigin/matrixcube/storage"
 	"github.com/matrixorigin/matrixcube/storage/executor/simple"
 	"github.com/matrixorigin/matrixcube/storage/kv"
-	"github.com/matrixorigin/matrixcube/storage/kv/mem"
+	"github.com/matrixorigin/matrixcube/storage/kv/pebble"
 	"github.com/matrixorigin/matrixcube/vfs"
 	"github.com/reusee/e4"
 	"github.com/reusee/fz"
@@ -56,17 +58,17 @@ func TestNewCubeCluster(t *testing.T) {
 		var configs []*config.Config
 		for i := 0; i < numNodes; i++ {
 			i := i
-			fs := vfs.NewMemFS()
 
 			logger, err := zap.NewDevelopment()
 			ce(err)
+			fs := vfs.Default
+
 			configs = append(configs, &config.Config{
-				Logger:             logger,
-				UseMemoryAsStorage: true,
-				FS:                 fs,
-				DataPath:           filepath.Join(string(tempDir), "foo"),
-				RaftAddr:           net.JoinHostPort("127.0.0.1", nextPort()),
-				ClientAddr:         net.JoinHostPort("127.0.0.1", nextPort()),
+				Logger:     logger,
+				FS:         fs,
+				DataPath:   filepath.Join(string(tempDir), "foo"),
+				RaftAddr:   net.JoinHostPort("127.0.0.1", nextPort()),
+				ClientAddr: net.JoinHostPort("127.0.0.1", nextPort()),
 				Labels: [][]string{
 					{"c", fmt.Sprintf("%d", i)},
 				},
@@ -93,6 +95,7 @@ func TestNewCubeCluster(t *testing.T) {
 						ClientUrls:       etcdClientEndpoints[i],
 						PeerUrls:         etcdPeerEndpoints[i],
 					},
+					DisableStrictReconfigCheck: true,
 				},
 
 				Replication: config.ReplicationConfig{
@@ -112,7 +115,8 @@ func TestNewCubeCluster(t *testing.T) {
 				},
 
 				Storage: func() config.StorageConfig {
-					kvStorage := storage.KVStorage(mem.NewStorage())
+					kvStorage, err := pebble.NewStorage(fs.PathJoin(string(tempDir), "data"), logger, &crdbpebble.Options{})
+					ce(err)
 					base := kv.NewBaseStorage(kvStorage, fs)
 					dataStorage := kv.NewKVDataStorage(base, simple.NewSimpleKVExecutor(kvStorage))
 					return config.StorageConfig{
@@ -133,6 +137,29 @@ func TestNewCubeCluster(t *testing.T) {
 		cluster, err := newCluster(configs)
 		ce(err)
 		defer closeCluster(cluster)
+
+	})
+}
+
+func TestCubeTestCluster(t *testing.T) {
+	defer he(nil, e4.TestingFatal(t))
+
+	NewCubeScope(
+		NewConfigScope(
+			NewGlobalScope(),
+		),
+	).Call(func() {
+
+		cluster := raftstore.NewTestClusterStore(t,
+			raftstore.WithTestClusterNodeCount(3),
+			raftstore.DiskTestCluster,
+		)
+		cluster.Start()
+		defer cluster.Stop()
+
+		cluster.WaitShardByCountPerNode(1, time.Second*10)
+		cluster.WaitLeadersByCount(1, time.Second*10)
+		cluster.CheckShardCount(1)
 
 	})
 }
